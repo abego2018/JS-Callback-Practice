23684c38e049154ac6eec9707e17e1df60911a31
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-02-04 21:22:52 -0800

    character moving

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 const inventory = newInventory()
-move(inventory).to(0, 0)
+move(inventory).to(0,0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250
-
-function moveCharacter(){
-    if (direction === 'west'){
-        x = x - 1
-    }
-
-    if (direction === 'north'){
-        y = y + 1
-    }
-
-    if (direction === 'east'){
-        x = x + 1
-    }
-
-    if (direction === 'south'){
-        y = y - 1
-    }
-
-    character.style.let = x + 'px'
-    character.style.let = y + 'px'
-}
-move(character).to(100, 250)
-setInterval(moveCharacter, 1)
+move(character).withArrowKeys(100, 250)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
@@ -37,4 +12,5 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
 
--- newInventory.js
+++ newInventory.js
@@ -7,7 +7,7 @@ function newInventory(){
     inventory.style.alignItems = 'center'
     inventory.style.justifyContent = 'space-evenly'
     inventory.style.border = '2px solid black'
-    inventory.style.backgroundColor = 'brown'
+    inventory.style.backgroundColor = 'gray'
     document.body.append(inventory)
     return inventory
 }
